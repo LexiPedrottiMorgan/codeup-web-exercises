27de97bff6bcb2163e87e75ebee01aace200c4f4
Lexi Morgan <leximorgan816@gmail.com>
2018-12-11 12:14:39 -0600

    adding the mouse events exercise- finished

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -12,15 +12,43 @@
 
 //Exercise 3: Element Selectors
 
-  $("li").css("font-size", "20px");
+  // $("li").css("font-size", "20px");
   // $("li").css("background-color", "yellow");
   // $("p").css("background-color", "yellow");
   // $("h1").css("background-color", "yellow");
 
-  alert($('h1').html());
+  // alert($('h1').html());
 
 //Exercise 4: Multiple Selectors
-  $("li, h1, p").css("background-color", "yellow");
+//   $("li, h1, p").css("background-color", "yellow");
+
+
+
+//Mouse events exercise:
+
+// Add jQuery code that will change the background color of a h1 element when clicked.
+
+    $("h1").click(function(){
+        $(this).css("background-color", "papayawhip");
+    });
+
+
+// Make all paragraphs have a font size of 18px when they are double clicked.
+
+     $("p").dblclick(function(){
+         $(this).css("font-size", "18px");
+     });
+
+
+// Set all li text color to red when the mouse is hovering, reset to black when it is not.
+
+     $("li").hover(function(){
+         $(this).css("color", "red");
+     }, function(){
+         $(this).css("color", "");
+     });
+
+
 
 
 
