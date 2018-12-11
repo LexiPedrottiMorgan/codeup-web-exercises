40596bd78a62d4e3ff6016c5a7012a0ce8be59c1
Lexi Morgan <leximorgan816@gmail.com>
2018-12-11 09:50:05 -0600

    jQuery exercise 1 and 2 finished.

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -1,8 +1,26 @@
 "use strict";
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+ $(document).ready(function() {
+     alert( 'The DOM has finished loading!' );
 
+//Exercise 1: Id selectors
 
+// var header = $('#fancy-header').html();
+// alert(header);
+
+// Exercise 2: class selectors
+// $(".codeup").css("border-style", "solid", "1px").css("border-color", "red");
+
+//Exercise 3: Element Selectors
+
+  $("li").css("font-size", "20px");
+  // $("li").css("background-color", "yellow");
+  // $("p").css("background-color", "yellow");
+  // $("h1").css("background-color", "yellow");
+
+  alert($('h1').html());
+
+//Exercise 4: Multiple Selectors
+  $("li, h1, p").css("background-color", "yellow");
 
 
 
